7dba61bea4c4bc5348ea75c47deea7e51b97cf73
gwenaelle <gwenaelle.duchesne@gmail.com>
2021-08-01 22:44:33 +0200

    header + tags

--- main.js
+++ main.js
@@ -13,15 +13,36 @@ function getPhotographers() {
     })
     .then(function(value) {
       console.log(value);
+      let allTags = [];
       value.photographers.forEach(photographer => {
         showPhotographers(photographer);
+        photographer.tags.forEach(tag => {
+          if (!allTags.includes(tag)) {
+            allTags.push(tag);
+          }
+        });
       });
+      let parent = document.querySelector(".header");
+      showTags(allTags, parent);
+      console.log(allTags);
     })
     .catch(function(err) {
       console.log(err);
     });
 }
 document.addEventListener("load", getPhotographers());
+
+function showTags(tags, parent) {
+  const tagList = document.createElement("ul");
+  tagList.classList.add("taglist");
+  tags.forEach(tag => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `#${tag}`;
+    listItem.classList.add("taglist__tag");
+    tagList.appendChild(listItem);
+  });
+  parent.appendChild(tagList);
+}
 
 function showPhotographers(photographer) {
   const card = document.createElement("article");
@@ -52,6 +73,8 @@ function showPhotographers(photographer) {
   card.appendChild(location);
   card.appendChild(tagLine);
   card.appendChild(price);
+
+  showTags(photographer.tags, card);
 
   const section = document.getElementById("photographers");
   section.appendChild(card);
