0343615988c90be18ed4d21a22f06a2ce2a368de
gwenaelle <gwenaelle.duchesne@gmail.com>
2021-08-13 17:50:25 +0200

    modified showPhotographers

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-function getJsonData() {
-  fetch("FishEyeData.json")
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
-
 function getPhotographers() {
   fetch("FishEyeData.json")
     .then(function(res) {
@@ -47,35 +41,16 @@ function showTags(tags, parent) {
 function showPhotographers(photographer) {
   const card = document.createElement("article");
   card.classList.add("card");
-  const link = document.createElement("a");
-  link.classList.add("card__link");
-  const img = document.createElement("img");
-  img.classList.add("card__img");
-  const name = document.createElement("h2");
-  name.classList.add("card__name");
-  const location = document.createElement("p");
-  location.classList.add("card__location");
-  const tagLine = document.createElement("p");
-  tagLine.classList.add("card__tagline");
-  const price = document.createElement("p");
-  price.classList.add("card__price");
-  const tags = document.createElement("ul");
-
-  img.src = `/public/img/Photographers-ID/${photographer.portrait}`;
-  name.textContent = photographer.name;
-  location.textContent = `${photographer.city}, ${photographer.country}`;
-  tagLine.textContent = photographer.tagline;
-  price.textContent = `${photographer.price}€/jour`;
-
-  card.appendChild(link);
-  link.appendChild(img);
-  link.appendChild(name);
-  card.appendChild(location);
-  card.appendChild(tagLine);
-  card.appendChild(price);
-
-  showTags(photographer.tags, card);
-
+  card.innerHTML = `
+    <a class="card__link">
+      <img class="card__img" src="/public/img/Photographers-ID/${photographer.portrait}">
+      <h2 class="card__name">${photographer.name}</h2>
+    </a>
+    <p class="card__location">${photographer.city}, ${photographer.country}</p>
+    <p class="card__tagline">${photographer.tagline}</p>
+    <p class="card__price">${photographer.price}€/jour</p>
+`;
   const section = document.getElementById("photographers");
   section.appendChild(card);
+  showTags(photographer.tags, card);
 }
